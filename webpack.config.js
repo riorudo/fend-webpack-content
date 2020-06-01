601c0f8c2df99908f8adb9a10279c17e4e7fd5d4
Michael Hochwieser <michael.hochwieser@gmail.com>
2020-06-01 17:17:22 +0200

    feat: add new loaders to webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,26 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-    entry: './src/client/index.js'
+    entry: './src/client/index.js',
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: ['babel-loader', 'eslint-loader']
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.json$/,
+                loader: 'json-loader'
+            }
+        ]
+    }
 }
